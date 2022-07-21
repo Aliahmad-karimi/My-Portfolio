d4d0bc1f75842de2f387a2604388cfa4b5fbdeac
Aliahmad Karimi <Aliahmad-karimi@users.noreply.github.com>
2022-07-21 15:48:53 +0430

    fix js linter errors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let menuBar = document.querySelector('.menuBar');
-let toolSocial = document.querySelector('.tool-social');
+const menuBar = document.querySelector('.menuBar');
+const toolSocial = document.querySelector('.tool-social');
 
-menuBar.addEventListener("click", () => {
-    menuBar.classList.toggle("active");
-    toolSocial.classList.toggle("active");
-})
+menuBar.addEventListener('click', () => {
+  menuBar.classList.toggle('active');
+  toolSocial.classList.toggle('active');
+});
 
-document.querySelectorAll(".Line").forEach(n => n.addEventListener("click", () => {
-    menuBar.classList.remove("active");
-    toolSocial.classList.remove("active");
-}))
+document.querySelectorAll('.Line').forEach(n, () => n.addEventListener('click', () => {
+  menuBar.classList.remove('active');
+  toolSocial.classList.remove('active');
+}));
