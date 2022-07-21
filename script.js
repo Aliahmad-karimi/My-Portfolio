3e0b9533521eee2faff792883e05f42dbfe68e06
mule <belmeetmule@gmail.com>
2022-07-21 13:28:32 +0300

    toggle humberger icon

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let toolSocial = document.querySelector('.tool-social');
 menuBar.addEventListener("click", () => {
     menuBar.classList.toggle("active");
     toolSocial.classList.toggle("active");
+    menuBar.style.display = "none";
 })
 
 document.querySelectorAll(".Line").forEach(n => n.addEventListener("click", () => {
     menuBar.classList.remove("active");
     toolSocial.classList.remove("active");
+    menuBar.style.display = "block";
 }))
