1f12a05ba032c0665ca7ee9054258e98e9c9b5c3
Osbentech <benmma13@gmail.com>
2022-07-25 18:00:20 -0700

    add object from index in js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 // This is menu bar and close button
+const menu = document.querySelector('.menuBar');
+const menuList = document.querySelector('.tool');
+const close = document.querySelector('.clos-button');
