94635225df08332af048068e2641af27b49e703e
Osbentech <benmma13@gmail.com>
2022-07-27 17:19:43 -0700

    add email lowercase validation to the form

--- script.js
+++ script.js
@@ -110,4 +110,24 @@ document.addEventListener('DOMContentLoaded', () => {
   popClose.addEventListener('click', () => {
     popUp.classList.remove('active');
   });
+  //validation of form.
+  const contForm = document.querySelector('form');
+  const emailForm = document.getElementById('email');
+  const vError = document.querySelector('.error')
+
+  function checkEmail(input) {
+    const low = input.toLowerCase();
+
+    if(input !== low){
+      vError.innerText = 'Please submit your Email in lower case letters with @ sign.';
+      vError.className = 'alert error';
+      return false;
+    }
+    if(input === low){
+      vError.innerText = 'Submitted Succesfully';
+      vError.className = 'alert success';
+      return true;
+    }
+  };
+
 });
